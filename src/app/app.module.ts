053ab4b3bdde9c19e1d05bbd5abdd6971e0bb8a1
Rohan Parkes <rparkes@email.com>
2020-03-11 23:48:11 +1100

    Finished app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { WeatherService } from './weather/weather.service';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent, CurrentWeatherComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [WeatherService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  appId: '71e6b4ee9adead3c9e754dcc8dba35d4',
+  baseUrl: 'http://',
 };
 
 /*
