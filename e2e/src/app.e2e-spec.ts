014e39e2e15e9d2c92f15410f85ac14522400dc9
Rohan Parkes <rparkes@email.com>
2020-03-12 18:52:18 +1100

    Delpoyment settings

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -14,18 +14,17 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('LocalCast Weather');
   });
 
-  /*
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser
       .manage()
       .logs()
       .get(logging.Type.BROWSER);
+
     expect(logs).not.toContain(
       jasmine.objectContaining({
         level: logging.Level.SEVERE,
       } as logging.Entry)
     );
   });
-  */
 });
