8bdf29df670841d25d990909eebce1dd1b882c75
Rohan Parkes <rparkes@email.com>
2020-03-30 20:55:00 +1100

    Changed to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appId: '71e6b4ee9adead3c9e754dcc8dba35d4',
-  baseUrl: 'http://',
+  baseUrl: 'https://',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appId: '71e6b4ee9adead3c9e754dcc8dba35d4',
-  baseUrl: 'http://',
+  baseUrl: 'https://',
 };
 
 /*
