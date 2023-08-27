81d0deef67ff4ee1b4a5c9e906dcdda1163c74e5
Rohan Parkes <rparkes@email.com>
2023-08-27 20:23:45 +1000

    Removed assets folder, fixed all unit tests.

--- src/app/city-search/city-search.component.spec.ts
+++ src/app/city-search/city-search.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CitySearchComponent } from './city-search.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { WeatherService } from '../weather/weather.service';
 import { WeatherServiceFake } from '../weather/weather.service.fake';
@@ -14,7 +16,12 @@ describe('CitySearchComponent', () => {
     TestBed.configureTestingModule({
       declarations: [CitySearchComponent],
       providers: [{ provide: WeatherService, useClass: WeatherServiceFake }],
-      imports: [MatFormFieldModule, ReactiveFormsModule],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+      ],
     });
     fixture = TestBed.createComponent(CitySearchComponent);
     component = fixture.componentInstance;
